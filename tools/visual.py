67b03d3146b6d0d885d2c5d4f51af4b7497ee34d
Chang Liu <ptg.cliu@u.northwestern.edu>
2022-07-31 20:50:31 -0500

    Figure formats & [Ca ii]

---
+++ tools/visual.py
@@ -0,0 +1,11 @@
+def rcparams_format(dic):
+    dic['text.usetex'] = True
+    dic['font.family'] = 'sans-serif'
+    dic['font.sans-serif'] = ['Ariel']
+    dic['font.size'] = 25
+    dic['xtick.labelsize'] = 25
+    dic['ytick.labelsize'] = 25
+    dic['xtick.major.size'] = 6
+    dic['ytick.major.size'] = 6
+    dic['xtick.minor.size'] = 2
+    dic['ytick.minor.size'] = 2
